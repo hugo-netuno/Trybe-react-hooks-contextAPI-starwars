862e8b94a49b85bdb3a587ae1c5c08eed832d294
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-01-28 16:04:40 -0300

    uploading

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Provider from './context/Provider';
+import FilterName from './components/FilterName';
+import NumericFilter from './components/NumericFilter';
+import Table from './components/Table';
+import './App.css';
+import FilterDisplay from './components/FilterDisplay';
+import FilterOrder from './components/FilterOrder';
+
+function App() {
+  return (
+    <Provider>
+      <FilterName />
+      <NumericFilter />
+      <FilterDisplay />
+      <FilterOrder />
+      <Table />
+    </Provider>
+  );
+}
+
+export default App;
